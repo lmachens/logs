5f2cbcee39483a12ee0f5fc46f8419acc70753dd
lmachens <leon.machens@googlemail.com>
2020-09-02 14:16:25 +0200

    Add LogsTable component

---
+++ client/src/components/LogsTable/LogsTable.stories.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import LogsTable from "./LogsTable";
+
+export default {
+  title: "LogsTable",
+  component: LogsTable,
+};
+
+const Template = (args) => <LogsTable {...args} />;
+
+export const Empty = Template.bind({});
+Empty.args = {
+  children: <div>No logs available</div>,
+};
+
+const logs = [
+  {
+    id: "1",
+    origin: "Trophy Hunter",
+    createdAt: "2020-09-02T11:50:29.679Z",
+    level: "error",
+    message: "Server crashed",
+  },
+  {
+    id: "2",
+    origin: "Trophy Hunter",
+    createdAt: "2020-09-03T11:50:29.679Z",
+    level: "warning",
+    message: "High Memory usage",
+  },
+  {
+    id: "3",
+    origin: "Server 2",
+    createdAt: "2020-09-02T12:50:29.679Z",
+    level: "info",
+    message: "Server crashed",
+  },
+  {
+    origin: "neuefische.de",
+    createdAt: "2020-09-02T11:50:29.679Z",
+    level: "error",
+    message: "Server crashed",
+    id: "SvG7lzQ",
+  },
+];
+export const Full = Template.bind({});
+Full.args = {
+  children: logs.map((log) => (
+    <tr key={log.id}>
+      <td>{log.origin}</td>
+      <td>{log.createdAt}</td>
+      <td>{log.level}</td>
+      <td>{log.message}</td>
+    </tr>
+  )),
+};
