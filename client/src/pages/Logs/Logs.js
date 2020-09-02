2fa26fa7ab8119ef11812f1cb61bfa6e8beb15be
lmachens <leon.machens@googlemail.com>
2020-09-02 15:37:05 +0200

    Implement API

--- client/src/pages/Logs/Logs.js
+++ client/src/pages/Logs/Logs.js
@@ -1,7 +1,22 @@
 import React from "react";
+import LogsTable from "../../components/LogsTable/LogsTable";
+import LogsTableRow from "../../components/LogsTableRow/LogsTableRow";
+import { getLogs } from "../../api/logs";
+import useAsync from "../../hooks/useFetch";
 
 const Logs = () => {
-  return <div>Logs</div>;
+  const { data: logs, loading } = useAsync(getLogs);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <LogsTable>
+      {logs?.map((log) => (
+        <LogsTableRow key={log.id} log={log} />
+      ))}
+    </LogsTable>
+  );
 };
 
 export default Logs;
