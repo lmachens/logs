8d1414678b9f9a25ebf64b7ff457683828901a1b
lmachens <leon.machens@googlemail.com>
2020-09-02 17:16:11 +0200

    Add POST logs

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require("express");
 const path = require("path");
 const { connectDB } = require("./lib/db");
 const logsRouter = require("./lib/logsRouter");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
