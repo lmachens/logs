fbd335252d9b502cdc8c841fcf17036bd747a690
lmachens <leon.machens@googlemail.com>
2020-09-02 13:54:47 +0200

    Add json-server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const jsonServer = require("json-server");
 
 const app = express();
 const port = process.env.PORT || 3001;
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -10,6 +13,14 @@ app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
 );
+
+app.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+app.use(router);
+app.use(middlewares);
 
 // Handle React routing, return all requests to React app
 app.get("*", (req, res) => {
