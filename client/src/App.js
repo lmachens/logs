728dfd89578c9f86c5fd131a46af14b83aba806a
lmachens <leon.machens@googlemail.com>
2020-09-02 15:19:30 +0200

    Add boilerplate pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Apps from "./pages/Apps";
+import Logs from "./pages/Logs";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
       <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Logs</Link>
+            </li>
+            <li>
+              <Link to="/apps">Apps</Link>
+            </li>
+          </ul>
+        </nav>
         <Switch>
-          <Route path="/apps">Apps</Route>
-          <Route path="/">Logs</Route>
+          <Route path="/apps">
+            <Apps />
+          </Route>
+          <Route path="/">
+            <Logs />
+          </Route>
         </Switch>
       </Router>
     </div>
