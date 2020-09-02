7c78ce4d35e01d132eb1f20367351b4499eebdbd
lmachens <leon.machens@googlemail.com>
2020-09-02 17:08:26 +0200

    Remove json-server

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
-const jsonServer = require("json-server");
 const { connectDB } = require("./lib/db");
 
 const app = express();
 const port = process.env.PORT || 3001;
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -16,14 +13,6 @@ app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
 );
-
-app.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
-app.use(router);
-app.use(middlewares);
 
 // Handle React routing, return all requests to React app
 app.get("*", (req, res) => {
