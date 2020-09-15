bf0a44f72f1e421fb8d42f044c4a1208f02c25a2
Leon Machens <leon.machens@googlemail.com>
2020-09-15 18:04:43 +0200

    Apply login page styling (#25)

---
+++ client/src/pages/Login/Login.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Login from "./Login";
+
+export default {
+  title: "Login",
+  component: Login,
+};
+
+const Template = (args) => <Login {...args} />;
+
+export const Default = Template.bind({});
