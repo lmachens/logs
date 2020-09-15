ba3e3577b632deb6de0222f6c91ca5e7a7933eae
lmachens <leon.machens@googlemail.com>
2020-09-15 13:32:07 +0200

    Add cookie-parser

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const { connectDB } = require("./lib/db");
 const logsRouter = require("./lib/logsRouter");
-const bodyParser = require("body-parser");
 const authRouter = require("./lib/authRouter");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
