6b48e3999b20eebd7b25b6bfa4031b520e9bdbe7
lmachens <leon.machens@googlemail.com>
2020-09-15 14:00:40 +0200

    Add Login page

---
+++ client/src/api/auth.js
@@ -0,0 +1,12 @@
+export const login = async (credentials) => {
+  const response = await fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  const account = await response.json();
+  return account;
+};
