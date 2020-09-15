6bc2b701084e8e11b5a683e6c6a47140748fbb81
lmachens <leon.machens@googlemail.com>
2020-09-15 17:17:36 +0200

    Apply login page styling

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,6 +1,36 @@
+import styled from "@emotion/styled";
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { login } from "../../api/auth";
+
+const Form = styled.form`
+  max-width: 400px;
+  margin: 0 auto;
+
+  input {
+    width: 100%;
+    margin: 0.8rem 0;
+    padding: 0.8rem 1rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  button {
+    width: 100%;
+    background-color: var(--active-color);
+    color: var(--base-color);
+    margin: 8px 0;
+    padding: 0.8rem 1rem;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+
+    :disabled {
+      cursor: default;
+      background-color: var(--disabled-color);
+    }
+  }
+`;
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -13,15 +43,16 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <input
         type="email"
         value={email}
         onChange={(event) => setEmail(event.target.value)}
         placeholder="Enter email"
+        autoFocus
       />
-      <button>Login</button>
-    </form>
+      <button disabled={email.trim().length === 0}>Login</button>
+    </Form>
   );
 };
 
