1080ce826f574dce8f7ff69980def4868f485140
lmachens <leon.machens@googlemail.com>
2020-09-15 12:34:15 +0200

    Add login and register routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const { connectDB } = require("./lib/db");
 const logsRouter = require("./lib/logsRouter");
 const bodyParser = require("body-parser");
+const authRouter = require("./lib/authRouter");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -19,6 +20,7 @@ app.use(
 );
 
 app.use("/api/logs", logsRouter);
+app.use("/api", authRouter);
 
 // Handle React routing, return all requests to React app
 app.get("*", (req, res) => {
